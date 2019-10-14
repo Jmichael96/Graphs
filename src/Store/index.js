2f547b1b6548b397cebc0b825818815aa3219d8f
Jeffrey <jeffrey.vanhorn@yahoo.com>
2019-10-14 18:54:35 -0500

    setup redux

---
+++ src/Store/index.js
@@ -0,0 +1,29 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import sagas from './sagas';
+import tubingReducer from './reducers/TubingPressure';
+import casingReducer from './reducers/CasingPressure';
+import flareReducer from './reducers/FlareTemp';
+import injValveReducer from './reducers/InjValveOpen';
+import oilTempReducer from './reducers/OilTemp';
+import waterTempReducer from './reducers/WaterTemp';
+
+export default () => {
+    const rootReducer = combineReducers({
+        tubing: tubingReducer,
+        casing: casingReducer,
+        flare: flareReducer,
+        injValve: injValveReducer,
+        oil: oilTempReducer,
+        water: waterTempReducer
+    });
+    const composeEnhancers = composeWithDevTools({});
+    const sagaMiddleware = createSagaMiddleware();
+    const middlewares = applyMiddleware(sagaMiddleware);
+    const store = createStore(rootReducer, composeEnhancers(middlewares));
+    
+    sagas.forEach(sagaMiddleware.run);
+
+    return store
+}
---
+++ src/Store/reducers/OilTemp.js
@@ -0,0 +1,27 @@
+import * as actions from "../actionTypes";
+
+const initialState = {
+    metric: "",
+    value: [],
+    unit: "",
+    at: "",
+    fullDate: []
+};
+
+export default (state = initialState, action) => {
+    const { updatedObject } = action;
+
+    switch (action.type) {
+        case actions.OIL_TEMP_RECIEVED:
+            return {
+                ...state,
+                metric: updatedObject.metric,
+                value: [...state.value, updatedObject.value],
+                unit: updatedObject.unit,
+                at: updatedObject.at,
+                fullDate: [...state.fullDate, updatedObject.fullDate],
+            };
+        default:
+            return state;
+    }
+}
